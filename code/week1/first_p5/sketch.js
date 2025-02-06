8465472e4d63ac60a77ee573d9b090d2727bf2d6
Jing Yu <jyu42@sva.edu>
2025-02-06 16:13:27 -0500

    Homework webpage

--- code/week1/first_p5/sketch.js
+++ code/week1/first_p5/sketch.js
@@ -1,8 +1,14 @@
 function setup() {
     createCanvas(400, 400);
     rectMode(CENTER);
+
+    btn = select('#my-button');
+    btn.mouseClicked(onBtnClicked);
   }
-  
+  function onBtnClicked(){
+    fill(random(255));
+    
+  }
   function draw() {
     background(220);
     circle(100,100,100);
